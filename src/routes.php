6f59413b9d0fd70cecc3f78c4228243863a9f32a
Luiz Pedro <luizpsmoreira@gmail.com>
2020-04-28 00:19:48 -0300

    photos

--- src/routes.php
+++ src/routes.php
@@ -15,14 +15,16 @@ $router->get('/logout', 'LoginController@logout');
 
 $router->post('/post/new', 'PostController@new');
 
+$router->get('/profile/{id}/photos', 'ProfileController@photos');
 $router->get('/profile/{id}/friends', 'ProfileController@friends');
 $router->get('/profile/{id}/follow', 'ProfileController@follow');
 $router->get('/profile/{id}', 'ProfileController@index');
+
 $router->get('/profile', 'ProfileController@index');
 
 $router->get('/friends', 'ProfileController@friends');
 
+$router->get('/photos', 'ProfileController@photos');
 
 // $router->get('/search', '');
-// $router->get('/photos', '');
 // $router->get('/settings', '');
